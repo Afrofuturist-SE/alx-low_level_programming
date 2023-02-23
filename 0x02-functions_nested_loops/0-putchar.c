30377fc54fd79823a44d4e710859fdc7faab82c6
Thato Moganedi <123355467+Afrofuturist-SE@users.noreply.github.com>
2023-02-23 11:16:31 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - print _putchar, followed by a new line
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
  * Return: Always 0
  */
 int main(void)
@@ -14,6 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
-	return (0);
+return (0);
 }
