414d570026d3b08e5830cc703a63b8d19022f119
Thato Moganedi <t.r.moganedi@gmail.com>
2023-02-23 04:55:26 -0800

    102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints first 50 Fibonacci numbers, 
+ * main - Prints first 50 Fibonacci numbers
  * starting with 1 and 2,
  * separated by a comma followed by a space.
  *
@@ -9,8 +9,7 @@
 int main(void)
 {
 	int count;
-  
-	unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
